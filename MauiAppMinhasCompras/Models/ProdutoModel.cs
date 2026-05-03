26d4d3de7931adee25e9b8bddbf57667fcb1b68d
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2026-05-03 12:11:14 -0300

    feat:Inclusaão de Model Protudo e troca da tela Inicial

--- MauiAppMinhasCompras/Models/ProdutoModel.cs
+++ MauiAppMinhasCompras/Models/ProdutoModel.cs
@@ -1,5 +1,16 @@
-﻿namespace MauiAppMinhasCompras.Models;
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models;
 
 public class ProdutoModel
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Descricao { get; set; } = string.Empty;
+
+    public double Preco { get; set; }
+
+    public double Quantidade { get; set; }
 }
