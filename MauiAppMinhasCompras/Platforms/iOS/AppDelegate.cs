355de3ed4d9ed6f8207668e49e7ab682aa9ab330
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2026-05-03 11:29:35 -0300

    Add project files.

---
+++ MauiAppMinhasCompras/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppMinhasCompras;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
